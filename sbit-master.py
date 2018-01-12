3df38774fd76ea8150ed2149546eb4b92723d0c4
Randy McClure <randymcclure75@gmail.com>
2018-01-11 20:41:02 -0700

    Completed more information gathering - Task 2.3.2

--- sbit-master.py
+++ sbit-master.py
@@ -1,15 +1,14 @@
 import boto3
 import re
+import getpass
 
 '''
-# DCs
-# FSs
-Instance Types
-Drive Sizes
 Admin Passwords
 '''
 MAX_DCS=8
 MIN_DCS=2
+MAX_VOLUME_SIZE=1500
+MIN_VOLUME_SIZE=10
 
 ec2 = boto3.resource('ec2') #EC2 object allows connection and manipulation of AWS EC2 resource types
 
@@ -22,7 +21,7 @@ def getDomainName():
         #The domain can include upper and lowercase letters, numbers, and
         #dashes (-), as long as the dashes are not the first or last 
         #character. Ex. "-example.com" = invalid, "ex-ample.com" = valid.
-        regex = re.compile('[A-Za-z0-9]([A-Za-z0-9-]*\.)+[A-Za-z0-9]+')
+        regex = re.compile('^[A-Za-z0-9]([A-Za-z0-9-]*\.)+[A-Za-z0-9]+$')
         if(regex.match(userDomain)):
             validDomain = True
         else:
@@ -55,15 +54,86 @@ def getNumDcs():
     while(not validNum):
         userNumDcs = input('How many Domain Controllers? [Leave blank to use default of 2]: ')
         #Ensure the user enters a number between 2 and 8, or leaves the input blank
-        regex = re.compile('(^$)|[2-8]')
+        regex = re.compile('(^$)|^[2-8]$')
         if(not regex.match(userNumDcs)):
             print('Please, enter a number between 2 and 8.')
         else:
             validNum = True
+    #Return the default if the user enters nothing
     if(userNumDcs == ''):
         return MIN_DCS
     else:
         return int(userNumDcs)
 
+#Prompt for the number of File Servers
+#Users must enter a number between 2 and 4, the default is 2
+def getNumFileServers():
+    validNum = False
+    #Loop until the user enters a number between 2 and 4, inclusive
+    while(not validNum):
+        userNumFileServers = input('How many File Servers? [Leave blank to use default of 2]: ')
+        #Ensure the user enters a number between 2 and 4, or leaves the input blank
+        regex = re.compile('(^$)|^[2-4]$')
+        if(not regex.match(userNumFileServers)):
+            print('Please, enter a number between 2 and 4.')
+        else:
+            validNum = True
+    #Return the default if the user enters nothing
+    if(userNumDcs == ''):
+        return MIN_DCS
+    else:
+        return int(userNumFileServers)
 
-print(getKeyPairName())
+#Prompt for and validate the size of drives to be added to file servers
+def getVolumeSize():
+    validSize = False
+    #Loop until a valid volume size is entered
+    while(not validSize):
+        userVolumeSize = input('How much storage would you like (in GiBs) on file servers?: ')
+        #Validate that the number entered is positive and between MAX_VOLUME_SIZE and MIN_VOLUME_SIZE
+        try:
+            if(int(userVolumeSize) > MAX_VOLUME_SIZE or int(userVolumeSize) < MIN_VOLUME_SIZE):
+                raise ValueError
+            else:
+                validSize = True
+        except ValueError:
+            print('Invalid input. Please enter a number between %d and %d.' % (MIN_VOLUME_SIZE,MAX_VOLUME_SIZE))
+    return int(userVolumeSize)
+
+#Prompt for and validate the instance type for instances
+def getInstanceType(message):
+    validTypes = ["t2.micro","t2.small","t2.medium","t2.large","t2.xlarge","t2.2xlarge","m5.large","m5.xlarge","m5.2xlarge","m5.4xlarge"]
+    validType = False
+    #Loop until a valid instance type is entered
+    while(not validType):
+        userInstanceType = input(message)
+        if(userInstanceType in validTypes):
+            validType = True
+        else:
+            print('Invalid input. Please enter one of the following instance types:')
+            for instanceType in validTypes[:-2]:
+                print('%s, ' % (instanceType), end='')
+            print(validTypes[-1])
+    return userInstanceType
+
+#Prompt for and validate the AD admin password
+def getPassword(message):
+    validPassword = False
+    #Loop until the user enters a password that is at least 8 characters and
+    #contains at least one upper and lowercase letter, number, and symbol
+    while(not validPassword):
+        userPassword = getpass.getpass(message)
+        #Check to see if password meets length and complexity requirements
+        if(
+            (len(userPassword) >= 8) and
+            (re.search(r'[A-Z]', userPassword) is not None) and #Checks for Uppercase
+            (re.search(r'[a-z]', userPassword) is not None) and #Checks for Lowercase
+            (re.search(r'\d', userPassword) is not None) and #Checks for Numbers
+            (re.search(r"[!#$%&'()*+,-./[\\\]^_`{|}~"+r'":;<=>?@]', userPassword) is not None) #Checks for Symbols
+           ):
+            validPassword = True
+        else:
+            print('Invalid password. Password should be 8 characters or more and contain an uppercase letter, lowercase letter, number, and symbol.')
+    return userPassword
+
+print(getPassword('Enter a password for the domain administrator account (separate account from the default "Administrator" account): '))
